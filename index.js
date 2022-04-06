48f8fa46e92a15356eda30736e1c97cf5f425118
Anastasia <nastena.pilipenko.01@gmail.com>
2022-04-06 14:44:06 +0300

    feat: add task_2

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function func1() {
     let color = document.getElementById("col").value
     document.body.style.backgroundColor = `${color}`
 }
+function func2() {
+    let color = document.querySelector(".selector").value
+    
+    document.body.style.backgroundColor = `${color}`
+}
