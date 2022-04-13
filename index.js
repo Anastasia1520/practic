60359ed22defd6d87bc2de66f73bb353ce38ba01
Anastasia <nastena.pilipenko.01@gmail.com>
2022-04-13 14:57:23 +0300

    feat: add task4

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 let auditorium = {
-    number: 57,
-    furniture: ['bookcase','desk','chair'],
-    tv: true,
-    video: true,
-    board: {
-        material: 'metal',
-        coating: ['green', 'matte'],
-    area: 70.80
-    }
-}
+  number: 57,
+  furniture: ["bookcase", "desk", "chair"],
+  tv: true,
+  video: true,
+  board: {
+    material: "metal",
+    coating: ["green", "matte"],
+  },
+  area: 70.8,
+  seats: 35,
+  addFurniture: function () {
+    this.furniture.push(document.getElementById("furniture").value);
+    document.getElementById("func1").textContent = this.furniture;
+  },
+  checkTv: () => {
+    this.tv = document.getElementById("checktv").checked
+      ? "Avaible"
+      : "Not avaible";
+    document.getElementById("tv").textContent = this.tv;
+  },
+  checkVideo() {
+    this.video = document.getElementById("checkvd").checked
+      ? "Avaible"
+      : "Not avaible";
+    document.getElementById("video").textContent = this.video;
+  },
+  showSeats() {
+    document.getElementById("countSeats").textContent = this.seats;
+  },
+  addSeats() {
+    document.getElementById("countSeats").textContent = ++this.seats;
+  },
+  removeSeats() {
+    document.getElementById("countSeats").textContent = --this.seats;
+  },
+};
 
-function addFurniture() {
-    auditorium.furniture.push(document.getElementById('furniture').value)
-    document.getElementById('func1').innerHTML = auditorium.furniture
-}
-
-function checkTv() {
-    auditorium.tv = document.getElementById('checktv').checked ? 'Имеется' : 'Не имеется'
-    document.getElementById('tv').innerHTML = auditorium.tv
-}
-
-function checkVideo() {
-    auditorium.video = document.getElementById('checkvd').checked ? 'Имеется' : 'Не имеется'
-    document.getElementById('video').innerHTML = auditorium.video
-}
-
-function changeMaterialBoard() {
-    auditorium.board.material = document.getElementById('inputMat').value
-    alert(`Материал доски: ${auditorium.board.material}`)
-}
+auditorium.changeMaterialBoard = function () {
+  auditorium.board.material = document.getElementById("inputMat").value;
+  alert(`Материал доски: ${auditorium.board.material}`);
+};
