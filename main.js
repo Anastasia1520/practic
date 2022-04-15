b8465329ce3eea4634c7e2c1fec1e6a95c85e75b
Anastasia <nastena.pilipenko.01@gmail.com>
2022-04-15 15:36:10 +0300

    feat: add task6.1

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function init() {
     textColorInput: document.querySelector("input.text-color-input"),
     addButton: document.querySelector("button.add-button"),
     items: document.querySelector("div.items"),
-    removeButton: document.querySelectorAll("button.delete-button"),
+    removeButton: document.querySelectorAll(".delete-button"),
   };
 
   render();
@@ -33,7 +33,7 @@ function render() {
     let div = document.createElement("div");
     div.className = "item";
     div.style.backgroundColor = itemsArray[i].bgColor;
-    items.append(div);
+    itms.append(div);
 
     let span = document.createElement("span");
     span.className = "item-text";
@@ -50,11 +50,12 @@ function render() {
 
 function removeItem() {
   let btns = document.querySelectorAll(".delete-button");
+  let colors = itemsArray.map((el) => el.color);
 
   btns.forEach((btn) => btn.addEventListener("click", removeItems));
-  btns.forEach((btn) => btn.removeEventListener("click", selectItem));
+  
   function removeItems() {
-    let colors = itemsArray.map((el) => el.color);
+   
     let index = colors.indexOf(`${this.parentNode.id}`);
     itemsArray.splice(index, 1);
     this.parentNode.remove();
@@ -77,4 +78,31 @@ function selectItem() {
     document.querySelector(".header").style.color =
       this.firstChild.style.color;
   }
+}
+function addItem() {
+  itemsArray.push({
+    color: htmlElements.textColorInput.value,
+    bgColor: htmlElements.bgColorInput.value,
+  });
+
+  lastChild = itemsArray.slice(-1);
+  let div = document.createElement("div");
+  div.className = "item";
+  div.id = lastChild[0].bgColor
+  div.style.backgroundColor = lastChild[0].bgColor;
+  itms.append(div);
+
+  let span = document.createElement("span");
+  span.className = "item-text";
+  span.style.color = lastChild[0].color;
+  span.textContent = `${lastChild[0].color}`;
+  div.append(span);
+
+  let btn = document.createElement("button");
+  btn.className = "delete-button";
+  btn.textContent = "X";
+  div.appendChild(btn);
+  removeItem()
+  selectItem()
+
 }
